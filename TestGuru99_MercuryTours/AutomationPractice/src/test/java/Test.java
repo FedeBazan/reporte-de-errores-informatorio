51dfb8c3979320bdc452079a62aef4e425829c59
FedeBazan <juliopablofedericobazan@gmail.com>
2023-11-24 12:14:09 -0300

    Update Test.java

--- TestGuru99_MercuryTours/AutomationPractice/src/test/java/Test.java
+++ TestGuru99_MercuryTours/AutomationPractice/src/test/java/Test.java
@@ -47,7 +47,7 @@ public class Test {
 
     /*Reporte 5 - way 1*/
     @org.junit.Test
-    public void runTestReport4_1(){
+    public void runTestReport5_1(){
         Home home = new Home(driver);
         LoginSucess loginSucess = new LoginSucess(driver);
         home.clearLoginInputs();
@@ -57,7 +57,7 @@ public class Test {
 
     /*Reporte 5 - way 2*/
     @org.junit.Test
-    public void runTestReport4_2(){
+    public void runTestReport5_2(){
         Home home = new Home(driver);
         SignOn signOn = new SignOn(driver);
         LoginSucess loginSucess =new LoginSucess(driver);
